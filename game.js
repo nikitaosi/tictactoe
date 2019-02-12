c05a4f08983bd600b11fcf52826e4b452c0c4847
Nikita Osipov <nikitaosipov.51@gmail.com>
2019-02-12 12:06:43 +0300

    update

--- game.js
+++ game.js
@@ -43,26 +43,26 @@ class playGame extends Phaser.Scene{
     create(){
         this.add.image(400, 450, 'background');
         this.boardArray = [];
-        for(var i = 0; i < gameOptions.boardSize.rows; i++){
-            this.boardArray[i] = [];
-            for(var j = 0; j < gameOptions.boardSize.cols; j++){
-                var tilePosition = this.getTilePosition(i, j);
+            for(var i = 0; j < 9; i++){
+                var newi = i/3;
+                var newj = i%3;
+                var tilePosition = this.getTilePosition(newi, newj);
                 var image = this.add.image(0, 0, "field").setInteractive();
-                image.setAlpha(0.5);
+                image.setAlpha(0.3);
                 var tilex = this.add.sprite(0, 0, "xo", 0).setInteractive();
                 var tileo = this.add.sprite(0, 0, "xo", 1);
                 tilex.visible = false;
                 tileo.visible = false;
                 var container = this.add.container (tilePosition.x, tilePosition.y, [ image, tilex, tileo ]);
-                this.input.on("pointerover", function (){
-                    image.setAlpha(1);
-                }, this);
-                this.input.on("pointerout", function (){
-                    image.setAlpha(0.5);
-                }, this);
-                this.input.on("pointerdown", function (){
-                    tilex.visible = true;
-                }, this);
+                image.on("pointerover", function (){
+                    this.setAlpha(0.6);
+                });
+                image.on("pointerout", function (){
+                    this.setAlpha(0.3);
+                });
+                image.on("pointerdown", function (){
+                    this.parentContainer.list[1].visible = true;
+                });
                 this.boardArray[i][j] = {
                     tileValue: 0,
                     tileSprite: tilex
@@ -70,32 +70,6 @@ class playGame extends Phaser.Scene{
             }
         }
         console.log(this.boardArray);
-        this.addTile();
-    }
-
-    handleTap(e){
-        console.log("You tapped in: " + e.x.toFixed(2) + ", " + e.y.toFixed(2));
-        e.setTint(0xff0000);
-    }
-
-    addTile(){
-        var emptyTiles = [];
-        for(var i = 0; i < gameOptions.boardSize.rows; i++){
-            for(var j = 0; j < gameOptions.boardSize.cols; j++){
-                if(this.boardArray[i][j].tileValue == 0){
-                    emptyTiles.push({
-                        row: i,
-                        col: j
-                    })
-                }
-            }
-        }
-        if(emptyTiles.length > 0){
-            var chosenTile = Phaser.Utils.Array.GetRandom(emptyTiles);
-            this.boardArray[chosenTile.row][chosenTile.col].tileValue = 1;
-            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.visible = true;
-            this.boardArray[chosenTile.row][chosenTile.col].tileSprite.setFrame(1);
-        }
     }
 
     getTilePosition(row, col){
